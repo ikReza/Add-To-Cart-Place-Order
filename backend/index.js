f031a88590f87f9a8ec6f22d2839b990279904b7
ikReza <ireza.kaiser00@gmail.com>
2020-05-21 14:43:51 +0600

    fourth commit

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,26 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
+const mongoose = require("mongoose");
 
 dotenv.config();
 const app = express();
 
+//DB
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(() => {
+    console.log("DB connected");
+  })
+  .catch((err) => {
+    console.log(`DB connection error: ${err.message}`);
+  });
+
+//Middlewares
 app.use(cors());
 app.use(express.json());
 app.use("/api", require("./routes/auth"));
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,9 +2,15 @@ const express = require("express");
 const router = express.Router();
 
 const data = require("../data-source");
+const Products = require("../models/Products");
 
-router.get("/products", (req, res) => {
-  res.json(data.products);
+router.get("/products", async (req, res) => {
+  try {
+    const products = await Products.find();
+    res.json(products);
+  } catch (err) {
+    res.status(400).json({ message: err });
+  }
 });
 
 module.exports = router;
